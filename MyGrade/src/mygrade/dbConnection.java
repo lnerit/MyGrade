1aa1431642856991ba4b0ada8b67f9623ab6791b
Lenz L Nerit <lewelnerit@gmail.com>
2017-10-08 18:20:00 +1000

    Refactored

--- MyGrade/src/mygrade/dbConnection.java
+++ MyGrade/src/mygrade/dbConnection.java
@@ -13,18 +13,17 @@ public Connection dbConn(){
    final String dbConnectionString="jdbc:sqlserver://localhost;user=sa;password=root;database=MyGrades";
 
 	try {
-		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-	} catch (ClassNotFoundException e1) {
-		System.out.print("Class Not Found...");
-	}
-	try {
-		Connection conn=DriverManager.getConnection(dbConnectionString);
-		if(conn!=null){
-			return conn;
-		}
-	} catch (SQLException e) {
-		System.out.println(e.getMessage());
-	}
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		
+			Connection conn=DriverManager.getConnection(dbConnectionString);
+			if(conn!=null){
+				return conn;
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
 	return null;
 }
 }
